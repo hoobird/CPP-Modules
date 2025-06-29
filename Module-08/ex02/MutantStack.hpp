f0b915b012303e1f596f861acdf9afc61f51d664
hoobird <hulim@student.42singapore.sg>
2025-06-29 21:12:02 +0800

    feat: enhance MutantStack and main.cpp with additional examples and improved iterator functionality

--- Module-08/ex02/MutantStack.hpp
+++ Module-08/ex02/MutantStack.hpp
@@ -1,38 +1,45 @@
 #ifndef MUTANTSTACK_HPP
 #define MUTANTSTACK_HPP
 
-
 #include <stack>
 #include <deque>
 #include <iterator>
 #include <iostream>
 
-template <typename T, class Container = std::deque<T> >
+template <typename T, typename Container = std::deque<T> >
 class MutantStack : public std::stack<T, Container>
 {
-public:
-    typedef std::stack<T, Container> StackT;
-    typedef typename Container::iterator iterator;
-    typedef typename Container::const_iterator const_iterator;
-    typedef typename Container::reverse_iterator reverse_iterator;
-    typedef typename Container::const_reverse_iterator const_reverse_iterator;
+    public:
+        typedef std::stack<T, Container> StackT;
+        typedef typename Container::iterator iterator;
+        typedef typename Container::const_iterator const_iterator;
+        typedef typename Container::reverse_iterator reverse_iterator;
+        typedef typename Container::const_reverse_iterator const_reverse_iterator;
 
-    MutantStack() : StackT() {}
-    MutantStack(const MutantStack& other) : StackT(other) {}
-    MutantStack& operator=(const MutantStack& other) {
-        StackT::operator=(other);
-        return *this;
-    }
-    ~MutantStack() {}
+        MutantStack() : StackT() {}
+        MutantStack(const MutantStack& other) : StackT(other) {}
+        MutantStack& operator=(const MutantStack& other) {
+            StackT::operator=(other);
+            return *this;
+        }
+        ~MutantStack() {}
 
-    iterator begin() { return this->c.begin(); }
-    iterator end() { return this->c.end(); }
-    const_iterator begin() const { return this->c.begin(); }
-    const_iterator end() const { return this->c.end(); }
-    reverse_iterator rbegin() { return this->c.rbegin(); }
-    reverse_iterator rend() { return this->c.rend(); }
-    const_reverse_iterator rbegin() const { return this->c.rbegin(); }
-    const_reverse_iterator rend() const { return this->c.rend(); }
+        iterator begin() { return this->c.begin(); }
+        iterator end() { return this->c.end(); }
+        const_iterator begin() const { return this->c.begin(); }
+        const_iterator end() const { return this->c.end(); }
+        reverse_iterator rbegin() { return this->c.rbegin(); }
+        reverse_iterator rend() { return this->c.rend(); }
+        const_reverse_iterator rbegin() const { return this->c.rbegin(); }
+        const_reverse_iterator rend() const { return this->c.rend(); }
 };
+
+std::ostream& operator<<(std::ostream& os, const MutantStack<int>& ms) {
+    os << "MutantStack contents: ";
+    for (MutantStack<int>::const_iterator it = ms.begin(); it != ms.end(); ++it) {
+        os << *it << " ";
+    }
+    return os;
+}
 
 #endif 
--- Module-08/ex02/main.cpp
+++ Module-08/ex02/main.cpp
@@ -2,19 +2,23 @@
 #include "iostream"
 #include <list>
 
-void mutantStackExample();
-void listExample();
+void mutantStackDefaultExample();
+void listDefaultExample();
+void mutantStackMyExample();
+void listMyExample();
 
 int main() {
 
     //comment what u dont want to run
-    mutantStackExample();
-    listExample();
+    mutantStackDefaultExample();
+    listDefaultExample();
+    mutantStackMyExample();
+    listMyExample();
     
     return 0;
 }
 
-void mutantStackExample() {
+void mutantStackDefaultExample() {
     std::cout << "\n=== Running MutantStack Example ===" << std::endl;
     MutantStack<int> mstack;
     mstack.push(5);
@@ -25,7 +29,6 @@ void mutantStackExample() {
     mstack.push(3);
     mstack.push(5);
     mstack.push(737);
-    //[...]
     mstack.push(0);
     MutantStack<int>::iterator it = mstack.begin();
     MutantStack<int>::iterator ite = mstack.end();
@@ -39,7 +42,7 @@ void mutantStackExample() {
     std::stack<int> s(mstack);
 }
 
-void listExample() {
+void listDefaultExample() {
     std::cout << "\n=== Running List Example ===" << std::endl;
     std::list<int> mlist;
     mlist.push_back(5);
@@ -50,7 +53,6 @@ void listExample() {
     mlist.push_back(3);
     mlist.push_back(5);
     mlist.push_back(737);
-    //[...]
     mlist.push_back(0);
     std::list<int>::iterator it = mlist.begin();
     std::list<int>::iterator ite = mlist.end();
@@ -62,4 +64,111 @@ void listExample() {
     ++it;
     }
     std::stack<int, std::list<int> > s(mlist);
+}
+
+void mutantStackMyExample() {
+    std::cout << "\n=== Running My MutantStack Example ===" << std::endl;
+    MutantStack<int> mstack;
+    for (int i = 42; i < 56; ++i) {
+        mstack.push(i);
+    }
+    std::cout << mstack << std::endl;
+    std::cout << "Size: " << mstack.size() << std::endl;
+    std::cout << "Top: " << mstack.top() << std::endl;
+    std::cout << std::endl;
+    const MutantStack<int> const_mstack(mstack);
+
+    std::cout << "Normal Iterator" << std::endl;
+    MutantStack<int>::iterator it = mstack.begin();
+    MutantStack<int>::iterator ite = mstack.end();
+    while (it != ite) {
+        std::cout << *it << " ";
+        ++it;
+    }
+    std::cout << std::endl;
+
+    std::cout << "Const Iterator" << std::endl;
+    MutantStack<int>::const_iterator cit = const_mstack.begin();
+    MutantStack<int>::const_iterator cite = const_mstack.end();
+    while (cit != cite) {
+        std::cout << *cit << " ";
+        ++cit;
+    }
+    std::cout << std::endl;
+
+    std::cout << "Reverse Iterator" << std::endl;
+    MutantStack<int>::reverse_iterator rit = mstack.rbegin();
+    MutantStack<int>::reverse_iterator rite = mstack.rend();
+    while (rit != rite) {
+        std::cout << *rit << " ";
+        ++rit;
+    }
+    std::cout << std::endl;
+
+    std::cout << "Const Reverse Iterator" << std::endl;
+    MutantStack<int>::const_reverse_iterator crit = const_mstack.rbegin();
+    MutantStack<int>::const_reverse_iterator crite = const_mstack.rend();
+    while (crit != crite) {
+        std::cout << *crit << " ";
+        ++crit;
+    }
+    std::cout << std::endl;
+    
+}
+
+std::ostream& operator<<(std::ostream& os, const std::list<int>& mlist) {
+    os << "List contents: ";
+    for (std::list<int>::const_iterator it = mlist.begin(); it != mlist.end(); ++it) {
+        os << *it << " ";
+    }
+    return os;
+}
+
+void listMyExample() {
+    std::cout << "\n=== Running My list Example ===" << std::endl;
+    std::list<int> mlist;
+    for (int i = 42; i < 56; ++i) {
+        mlist.push_back(i);
+    }
+    std::cout << mlist << std::endl;
+    std::cout << "Size: " << mlist.size() << std::endl;
+    std::cout << "Top: " << mlist.back() << std::endl;
+    std::cout << std::endl;
+    const std::list<int> const_mlist(mlist);
+
+    std::cout << "Normal Iterator" << std::endl;
+    std::list<int>::iterator it = mlist.begin();
+    std::list<int>::iterator ite = mlist.end();
+    while (it != ite) {
+        std::cout << *it << " ";
+        ++it;
+    }
+    std::cout << std::endl;
+
+    std::cout << "Const Iterator" << std::endl;
+    std::list<int>::const_iterator cit = const_mlist.begin();
+    std::list<int>::const_iterator cite = const_mlist.end();
+    while (cit != cite) {
+        std::cout << *cit << " ";
+        ++cit;
+    }
+    std::cout << std::endl;
+
+    std::cout << "Reverse Iterator" << std::endl;
+    std::list<int>::reverse_iterator rit = mlist.rbegin();
+    std::list<int>::reverse_iterator rite = mlist.rend();
+    while (rit != rite) {
+        std::cout << *rit << " ";
+        ++rit;
+    }
+    std::cout << std::endl;
+
+    std::cout << "Const Reverse Iterator" << std::endl;
+    std::list<int>::const_reverse_iterator crit = const_mlist.rbegin();
+    std::list<int>::const_reverse_iterator crite = const_mlist.rend();
+    while (crit != crite) {
+        std::cout << *crit << " ";
+        ++crit;
+    }
+    std::cout << std::endl;
 }
