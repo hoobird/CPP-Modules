d278a3f00e8685ee327c12f50e8229d816b65f95
Hubert Lim <hubertlimshingyi@gmail.com>
2025-06-29 18:14:33 +0800

    feat: add MutantStack class implementation and example usage in main.cpp

---
+++ Module-08/ex02/MutantStack.hpp
@@ -0,0 +1,38 @@
+#ifndef MUTANTSTACK_HPP
+#define MUTANTSTACK_HPP
+
+
+#include <stack>
+#include <deque>
+#include <iterator>
+#include <iostream>
+
+template <typename T, class Container = std::deque<T> >
+class MutantStack : public std::stack<T, Container>
+{
+public:
+    typedef std::stack<T, Container> StackT;
+    typedef typename Container::iterator iterator;
+    typedef typename Container::const_iterator const_iterator;
+    typedef typename Container::reverse_iterator reverse_iterator;
+    typedef typename Container::const_reverse_iterator const_reverse_iterator;
+
+    MutantStack() : StackT() {}
+    MutantStack(const MutantStack& other) : StackT(other) {}
+    MutantStack& operator=(const MutantStack& other) {
+        StackT::operator=(other);
+        return *this;
+    }
+    ~MutantStack() {}
+
+    iterator begin() { return this->c.begin(); }
+    iterator end() { return this->c.end(); }
+    const_iterator begin() const { return this->c.begin(); }
+    const_iterator end() const { return this->c.end(); }
+    reverse_iterator rbegin() { return this->c.rbegin(); }
+    reverse_iterator rend() { return this->c.rend(); }
+    const_reverse_iterator rbegin() const { return this->c.rbegin(); }
+    const_reverse_iterator rend() const { return this->c.rend(); }
+};
+
+#endif 
